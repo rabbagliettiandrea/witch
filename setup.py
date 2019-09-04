4483d9c88a5add493cedbaefef4c3caeca363c75
Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>
2019-09-04 18:12:04 +0200

    boto3 dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     install_requires=[
         'invoke==1.2.0',
         'slackclient~=2.0', 
-        'termcolor==1.1.0'
+        'termcolor==1.1.0',
+        'boto3~=1.9'
     ],
     entry_points={
         'console_scripts': ['witch = witch.main:program.run']
