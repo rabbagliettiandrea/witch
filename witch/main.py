98c32adcd4c2a6e23386922baf246fbf621ec680
Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>
2020-01-23 17:59:37 +0100

    fix

--- witch/main.py
+++ witch/main.py
@@ -1,13 +1,14 @@
 from invoke import Collection, Program
 
 from witch import VERSION
-from .tasks import dev, prod, utils, db
+from .tasks import dev, prod, utils, db, aws
 
 namespace = Collection()
 namespace.add_collection(prod)
 namespace.add_collection(dev)
 namespace.add_collection(db)
 namespace.add_collection(utils)
+namespace.add_collection(aws)
 namespace.add_task(prod.deploy)
 
 program = Program(namespace=namespace, version=VERSION)
