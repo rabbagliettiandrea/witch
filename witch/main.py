595d4f6d8a67929a0714e7bb57060cd9e9ef383f
Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>
2020-06-16 17:54:01 +0200

    Update main.py

--- witch/main.py
+++ witch/main.py
@@ -11,5 +11,6 @@ namespace.add_collection(utils)
 namespace.add_collection(aws)
 namespace.add_task(prod.deploy)
 namespace.add_task(prod.logs)
+namespace.add_task(prod.exec)
 
 program = Program(namespace=namespace, version=VERSION)
