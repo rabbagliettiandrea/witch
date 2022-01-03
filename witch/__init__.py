05ab1a9de45938388fb4bdf66104be8b9a9d87df
michelematera <49020600+michelematera@users.noreply.github.com>
2022-01-03 10:28:09 +0100

    Update __init__.py

--- witch/__init__.py
+++ witch/__init__.py
@@ -1,10 +1,13 @@
 import os
 from pathlib import Path
 import sys
+from dotenv import dotenv_values
 
 VERSION = '0.1.dev-1'
 
-PROJECT_NAME = os.path.basename(os.path.normpath(Path().absolute()))
+config = dotenv_values('.env')
+
+PROJECT_NAME = config.get('WITCH_PROJECT_NAME') or os.path.basename(os.path.normpath(Path().absolute()))
 
 sys.path.append('.')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{PROJECT_NAME}.settings')
