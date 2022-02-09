ad29b837aaf67a7ddf580ce2650142ddb6da0e9d
michelematera <49020600+michelematera@users.noreply.github.com>
2022-02-09 16:43:43 +0100

    Update dev.py

--- witch/tasks/dev.py
+++ witch/tasks/dev.py
@@ -2,20 +2,7 @@ from invoke import task
 
 from .utils import print_task_done
 
-DOCKER_MACHINE_ENV = {
-    'DOCKER_TLS_VERIFY': '',
-    'DOCKER_HOST': '',
-    'DOCKER_CERT_PATH': '',
-    'DOCKER_MACHINE_NAME': ''
-}
-
-_DOCKER_COMPOSE_COMMAND = 'docker-compose --env-file ./.env -f ./docker/docker-compose.dev.yml'
-
-
 @task
 def start_db(ctx):
-    ctx.run(
-        '{} up -d --build --remove-orphans'.format(_DOCKER_COMPOSE_COMMAND),
-        env=DOCKER_MACHINE_ENV
-    )
+    ctx.run('docker-compose --env-file ./.env up -d --build --remove-orphans')
     print_task_done()
