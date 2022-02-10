0d2f69311f00e91e8099a28d90cda35153b94c93
michelematera <49020600+michelematera@users.noreply.github.com>
2022-02-10 09:19:54 +0100

    Update dev.py

--- witch/tasks/dev.py
+++ witch/tasks/dev.py
@@ -4,5 +4,5 @@ from .utils import print_task_done
 
 @task
 def start_db(ctx):
-    ctx.run('docker-compose --env-file ./.env up -d --build --remove-orphans')
+    ctx.run('docker-compose up -d --build --remove-orphans')
     print_task_done()
