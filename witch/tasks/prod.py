ef1eb28ccd5518d42b7c620d7fd19d7bc3088354
michelematera <49020600+michelematera@users.noreply.github.com>
2022-02-11 10:22:06 +0100

    Update prod.py

--- witch/tasks/prod.py
+++ witch/tasks/prod.py
@@ -1,36 +1,10 @@
 from invoke import task
 from django.conf import settings
 from witch.tasks import aws, utils
-import paramiko
 
-WITCH_HOST_NAMES = getattr(settings, 'WITCH_K8S_NODES', [])
+WITCH_K8S_NODES = getattr(settings, 'WITCH_K8S_NODES', [])
 
-WITCH_HOST_USER = getattr(settings, 'WITCH_SSH_USER', None)
-
-
-def get_ssh_client():
-    for host in WITCH_K8S_NODES:
-        try:
-            ssh_client = paramiko.client.SSHClient()
-            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh_client.remote_host = host
-            ssh_client.connect(hostname=host, username=WITCH_SSH_USER)
-            return ssh_client
-        except:
-            continue
-
-    utils.print_error('No hosts available')
-    utils.abort()
-
-
-def get_pod_name(ssh_client, service):
-    try:
-        return ssh_client.exec_command(
-            'kubectl get pods -l app={} -o jsonpath="{{.items[0].metadata.name}}"'.format(service)
-        )[1].read().decode('utf-8').strip()
-    except:
-        utils.print_error('Pod name not found')
-        utils.abort()
+WITCH_SSH_USER = getattr(settings, 'WITCH_SSH_USER', None)
 
 
 @task
@@ -39,18 +13,23 @@ def exec(ctx, service='django', command='bash'):
         utils.print_error('WITCH_SSH_USER setting not found.')
         utils.abort()
 
-    ssh_client = get_ssh_client()
-    pod_name = get_pod_name(ssh_client, service)
-    ctx.run(
-        'ssh -t {}@{} -C "kubectl exec -it {} -- {}"'.format(
-            WITCH_SSH_USER,
-            ssh_client.remote_host,
-            pod_name,
-            command
-        ),
-        pty=True
-    )
+    for node in WITCH_K8S_NODES:
+        try:
+            ctx.run(
+                'ssh -t {}@{} -C "kubectl exec -it {} -- {}"'.format(
+                    WITCH_SSH_USER,
+                    node,
+                    '\$(kubectl get pods -l app={} -o jsonpath="{{.items[0].metadata.name}}")'.format(service),
+                    command
+                ),
+                pty=True, warn=False
+            )
+            return
+        except:
+            continue
 
+    utils.print_error('No nodes available')
+    utils.abort()
 
 @task
 def shell(ctx, service='django'):
