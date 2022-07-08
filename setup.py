25601b1ff096d88d33b291cbe3e9a8563bd28faf
michelematera <49020600+michelematera@users.noreply.github.com>
2022-07-08 10:44:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'invoke==1.2.0',
         'termcolor==1.1.0',
         'boto3~=1.9',
-        'python-dotenv==0.15.0'
+        'python-dotenv==0.20.0'
     ],
     entry_points={
         'console_scripts': ['witch = witch.main:program.run']
