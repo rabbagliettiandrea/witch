853cbf70a01c50e31d4649ee3827ee0fd395e332
michelematera <49020600+michelematera@users.noreply.github.com>
2022-07-08 15:16:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+import os
+import re
+import codecs
 from setuptools import setup, find_packages
 
-from witch import VERSION
+def read(package_name, file_name):
+    filename = os.path.join(os.path.dirname(__file__), package_name, file_name)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+def find_version(package_name, file_name):
+    version_file = read(package_name, file_name)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
 
 setup(
     name='witch',
-    version=VERSION,
+    version=find_version("witch", "__init__.py"),
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/rabbagliettiandrea/witch',
